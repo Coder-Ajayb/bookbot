890eabf595cd0d4e9e7112a365f6693b6f9be098
Coder-Ajayb <ajaybechoe@gmail.com>
2025-02-13 16:06:17 +0100

    Added def count_characters

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def count_words(text):
  return len(text.split())
 
+def count_characters(text):
+    char_count = {}
+    text = text.lower()
+
+    for char in text:
+      if char in char_count:
+         char_count[char] += 1
+      else:
+         char_count[char] = 1
+
+    return char_count
+
 def main():
     path_to_file = "books/frankenstein.txt"
 
@@ -8,6 +20,6 @@ def main():
         file_contents = f.read()
 
     print (f"Word count: {count_words(file_contents)}")
-
+    print ("Character count:", count_characters(file_contents))
 if __name__ == "__main__":
     main()
